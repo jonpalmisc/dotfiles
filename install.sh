8159158402a6cc7258e93f9426beae13a9752413
Jon Palmisciano <jp@jonpalmisc.com>
2021-02-16 17:34:03 -0500

    feat: added shell config notice

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ ln -s "$(pwd)/.tmux.conf" $HOME
 echo 'Warning: Without TPM, the linked tmux configuration will not work!'
 echo 'See https://github.com/tmux-plugins/tpm for installation instructions.'
 
-echo '\nDone!'
+echo '\nDone!\n'
+
+echo 'Make sure to add the following to your profile:\n'
+echo '  source ~/.aliases'
+echo '  source ~/.functions'

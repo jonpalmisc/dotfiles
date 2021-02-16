55b913ce5f14d9896189ea6a651c9f131bede148
Jon Palmisciano <jp@jonpalmisc.com>
2021-02-16 17:31:51 -0500

    feat: added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+rm "$HOME/.aliases" $HOME
+rm "$HOME/.functions" $HOME
+rm "$HOME/.tmux.conf" $HOME
+
+echo '\nDone!\n'
+
+echo 'Make sure to remove the following from your profile:\n'
+echo '  source ~/.aliases'
+echo '  source ~/.functions'

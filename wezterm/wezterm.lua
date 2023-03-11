ece67184b8f52ca8df7a6697e7371edd45a1b7ff
Jon Palmisciano <jp@jonpalmisc.com>
2023-03-11 12:42:05 -0500

    wezterm: Hide tab bar when only one tab is present

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,5 +3,8 @@ local wezterm = require "wezterm"
 return {
 	font = wezterm.font "Berkeley Mono",
 	color_scheme = "Catppuccin Macchiato",
+
 	use_ime = false, -- Interpret C-q, etc. correctly
+
+	hide_tab_bar_if_only_one_tab = true,
 }

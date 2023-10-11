c1289cf9516ecc3a707bee9a1b4b7b10853b20a9
Jon Palmisciano <jp@jonpalmisc.com>
2023-10-10 22:11:28 -0400

    wezterm: Use custom color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,45 @@
 local wezterm = require "wezterm"
 
+-- Must be manually synced with Industrial theme!
+local industrial_colors = {
+	black = "#000000",
+	grey = "#666666",
+	white = "#EEEEEE",
+
+	red = "#F0866F",
+	green = "#7FDA95",
+	yellow = "#E7B00D",
+	blue = "#7DC0EE",
+	purple = "#DF92D1",
+	cyan = "#73D0CE",
+}
+
+local ansi_colors = {
+	industrial_colors.grey,
+	industrial_colors.red,
+	industrial_colors.green,
+	industrial_colors.yellow,
+	industrial_colors.blue,
+	industrial_colors.purple,
+	industrial_colors.cyan,
+	industrial_colors.white,
+}
+
 return {
-	font = wezterm.font "Berkeley Mono",
-	color_scheme = "Catppuccin Macchiato",
+	font = wezterm.font "MD IO",
 
 	use_ime = false, -- Interpret C-q, etc. correctly
+
+	colors = {
+		background = industrial_colors.black,
+		foreground = industrial_colors.white,
+
+		cursor_bg = industrial_colors.white,
+		cursor_fg = industrial_colors.black,
+
+		ansi = ansi_colors,
+		brights = ansi_colors,
+	},
 
 	hide_tab_bar_if_only_one_tab = true,
 }

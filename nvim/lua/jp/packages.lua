6347b0b03b26f7c6ff8449052e79e0da7d2fd3ae
Jon Palmisciano <jp@jonpalmisc.com>
2023-11-05 13:40:01 -0500

    nvim: Migrate to `lazy.nvim` for package management

--- nvim/lua/jp/packages.lua
+++ nvim/lua/jp/packages.lua
@@ -1,86 +1,58 @@
-local ensure_packer = function()
-	local fn = vim.fn
-
-	local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system {
-			"git",
-			"clone",
-			"--depth",
-			"1",
-			"https://github.com/wbthomason/packer.nvim",
-			install_path,
-		}
-
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-
-	return false
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	}
 end
 
-local is_fresh_install = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
-function packer_config(use)
-	use "wbthomason/packer.nvim"
+require("lazy").setup {
+	"cohama/lexima.vim", -- Auto-close parentheses, etc.
+	"tpope/vim-sleuth", -- Auto-detect indentation preferences
+	"numToStr/Comment.nvim", -- Rapid (un)commenting
 
-	use "lewis6991/impatient.nvim"
-
-	--== Basic Editing ==---------------------------------------------------
-
-	use "cohama/lexima.vim" -- Auto-close parentheses, etc.
-	use "tpope/vim-sleuth" -- Auto-detect indentation preferences
-
-	-- Rapid (un)commenting helpers
-	use "numToStr/Comment.nvim"
-
-	--== Autocompletion & Snippets ==---------------------------------------
-
-	use {
+	{
+		-- Code completion engine
 		"hrsh7th/nvim-cmp",
-		requires = {
+		dependencies = {
 			"hrsh7th/cmp-nvim-lsp", -- Use LSP suggestions
 			"hrsh7th/cmp-nvim-lua", -- Auto-complete Neovim Lua APIs
 			"hrsh7th/cmp-buffer", -- Suggest words in the buffer
 			"hrsh7th/cmp-path", -- Complete local filesystem paths
 		},
-	}
+	},
 
-	use {
+	{
+		-- Snippet engine & default snippets
 		"L3MON4D3/LuaSnip",
-		requires = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
-	}
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
 
-	--== IDE Features ==----------------------------------------------------
+	"neovim/nvim-lspconfig", -- LSP support
 
-	use "neovim/nvim-lspconfig"
-	use {
+	{
+		-- Better syntax highlighting
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	}
+		build = ":TSUpdate",
+	},
 
-	use {
+	{
+		-- Fuzzy finder UI
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	}
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-	--==--------------------------------------------------------------------
-
-	use 'rktjmp/lush.nvim'
-	use '~/Developer/Source/Other/industrial'
-
-	if is_fresh_install then
-		require("packer").sync()
-	end
-end
-
--- Attempt to load packer; if it still cannot be loaded (fatal config issue),
--- stop loading this config here.
-local ok, packer = pcall(require, "packer")
-if not ok then
-	return
-end
-
--- Load Packer and plugins.
-packer.startup(packer_config)
+	"rktjmp/lush.nvim", -- Theme-building platform
+	"rktjmp/shipwright.nvim", -- Export functionality for Lush
+	{ dir = "~/Developer/Source/Other/industrial" }, -- Personal theme
+}

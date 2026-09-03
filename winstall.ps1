acd1713c390e33badf2dbb9997625fe7a84d5f27
Jon Palmisciano <jp@jonpalmisc.com>
2026-09-02 21:14:33 -0700

    meta(win): Remove config on install if already present

--- winstall.ps1
+++ winstall.ps1
@@ -6,43 +6,45 @@ param(
 
 $RepoRoot = $PSScriptRoot
 
+function Remove-IfExists {
+    param([string]$Path)
+
+    if (Get-Item -LiteralPath $Path -Force -ErrorAction SilentlyContinue) {
+        Remove-Item -LiteralPath $Path -Recurse -Force
+    }
+}
+
 function New-Junction {
     param([string]$LinkPath, [string]$TargetPath)
 
-    if (Test-Path $LinkPath) {
-        Remove-Item -Recurse -Force $LinkPath
-    }
-
+    Remove-IfExists $LinkPath
     New-Item -ItemType Junction -Path $LinkPath -Target $TargetPath | Out-Null
 }
 
-function New-FileSymbolicLink {
+function New-FileLink {
     param([string]$LinkPath, [string]$TargetPath)
 
     $LinkDirectory = Split-Path -Parent $LinkPath
     New-Item -ItemType Directory -Path $LinkDirectory -Force | Out-Null
 
-    if (Test-Path $LinkPath) {
-        Remove-Item -Force $LinkPath
-    }
-
+    Remove-IfExists $LinkPath
     New-Item -ItemType SymbolicLink -Path $LinkPath -Target $TargetPath | Out-Null
 }
 
 switch ($Target) {
     "nvim" {
-        New-Junction "$env:LOCALAPPDATA\nvim" "$RepoRoot\nvim"
+        New-DirLink "$env:LOCALAPPDATA\nvim" "$RepoRoot\nvim"
     }
     "subl" {
-        New-Junction "$env:APPDATA\Sublime Text\Packages\User" "$RepoRoot\subl"
+        New-DirLink "$env:APPDATA\Sublime Text\Packages\User" "$RepoRoot\subl"
     }
     "smerge" {
-        New-Junction "$env:APPDATA\Sublime Merge\Packages\User" "$RepoRoot\smerge"
+        New-DirLink "$env:APPDATA\Sublime Merge\Packages\User" "$RepoRoot\smerge"
     }
     "ps7" {
-        New-FileSymbolicLink "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "$RepoRoot\ps7\Microsoft.PowerShell_profile.ps1"
+        New-FileLink "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "$RepoRoot\ps7\Microsoft.PowerShell_profile.ps1"
     }
     "winterm" {
-        New-FileSymbolicLink "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$RepoRoot\winterm\settings.json"
+        New-FileLink "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$RepoRoot\winterm\settings.json"
     }
 }

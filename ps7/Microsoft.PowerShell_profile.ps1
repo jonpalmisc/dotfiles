5a5192f8bebd0c7811d701f42d058580aac08138
Jon Palmisciano <jp@jonpalmisc.com>
2026-09-02 21:11:38 -0700

    ps7: Use Emacs bindings, fix directory inheritance

--- ps7/Microsoft.PowerShell_profile.ps1
+++ ps7/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,24 @@
+Set-PSReadLineOption -EditMode Emacs
+
 Set-Alias ll ls
 
 Set-Alias g git
+
+function prompt {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+    $out = ""
+
+    # Communicates the working directory to Windows Terminal so that inheriting
+    # the previous directory when duplicating tabs works correctly.
+    if ($loc.Provider.Name -eq "FileSystem") {
+        $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+
+    $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+
+    return $out
+}
 
 function touch {
     param($File)
